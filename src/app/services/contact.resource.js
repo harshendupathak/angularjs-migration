b694c41567433a97843e93139d6ed041e5e3fa4e
harshendupathak <harshendupathak1998@mail.com>
2021-04-05 18:51:33 +0530

    updated

---
+++ src/app/services/contact.resource.js
@@ -0,0 +1,14 @@
+angular
+  .module("codecraft")
+  .factory("Contact", function($resource) {
+    return $resource(
+      "http://localhost:3000/contacts/:id",
+      {id: "@id"},
+      {
+        update: {
+          method: "PUT"
+        }
+      }
+    );
+  })
+  
